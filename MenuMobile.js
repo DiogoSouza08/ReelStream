7806f17594489fd3e6beb39a0b66c440bb1d9d05
Diogo Souza <115508662+DiogoSouza08@users.noreply.github.com>
2024-01-30 13:46:46 -0300

    Add files via upload

---
+++ MenuMobile.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const openMenuButton = document.getElementById('OpenMenu');
+    const closeMenuButton = document.getElementById('CloseMenu');
+    const menuContainer = document.getElementById('Menu');
+
+    openMenuButton.addEventListener('click', function () {
+        menuContainer.classList.remove('-translate-x-full');
+    });
+
+    closeMenuButton.addEventListener('click', function () {
+        menuContainer.classList.add('-translate-x-full');
+    });
+});
