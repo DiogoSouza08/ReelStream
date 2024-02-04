900494778f928c810c4764905fa7006f9d81ffc1
Diogo Souza <115508662+DiogoSouza08@users.noreply.github.com>
2024-02-04 19:35:17 -0300

    Update Top10filter.js

--- Top10filter.js
+++ Top10filter.js
@@ -3,19 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const filmes = document.querySelectorAll('.filme');
     const series = document.querySelectorAll('.serie');
 
-    // Função para alternar a visibilidade das imagens
+
     function toggleVisibilidade(elementos, mostrar) {
         elementos.forEach(elemento => {
             elemento.classList.toggle('hidden', !mostrar);
         });
     }
 
-    // Adiciona um ouvinte de eventos de mudança ao select
     tipoSelecao.addEventListener('change', function () {
         // Verifica qual opção foi selecionada
         const mostrarFilmes = tipoSelecao.value === '1';
 
-        // Mostra ou esconde as imagens com base na opção selecionada
+
         toggleVisibilidade(filmes, mostrarFilmes);
         toggleVisibilidade(series, !mostrarFilmes);
     });
