e3db84b7b2b7a73e0fe9e2961f9fad94fb0560a4
Diogo Souza <115508662+DiogoSouza08@users.noreply.github.com>
2024-02-23 16:05:32 -0300

    Update cadastro.js

--- Cadastro/cadastro.js
+++ Cadastro/cadastro.js
@@ -10,3 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+document.getElementById("criar-conta").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+
+    if (email.trim() === '' || password.trim() === '' || confirmPassword.trim() === '') {
+        alert("Por favor, preencha todos os campos antes de prosseguir.");
+    } else if (password.length < 8) {
+        alert("A senha deve ter pelo menos 8 caracteres.");
+    } else if (password !== confirmPassword) {
+        alert("As senhas não coincidem. Por favor, verifique e tente novamente.");
+    } else {
+        window.location.href = "../Pagamento/pagamento.html";
+    }
+});
+
